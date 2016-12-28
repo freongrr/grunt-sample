942f15fe390e381802374324e9d5b2a421d9813a
Fabien Cortina <fabien.cortina@gmail.com>
2016-12-28 15:32:27 +0900

    Add close button to hide error
    This is useless because the user has to refresh the page anyway.

--- java-react-webapp/src/main/webapp/ErrorDialog.js
+++ java-react-webapp/src/main/webapp/ErrorDialog.js
@@ -2,28 +2,44 @@
 "use strict";
 
 import React from "react";
-import {Modal} from "react-bootstrap";
+import {Modal, Button} from "react-bootstrap";
 
 type ErrorDialogProps = {
     error: Error
 };
 
+type ErrorDialogState = {
+    visible: boolean
+};
+
 export default class ErrorDialog extends React.Component {
     props: ErrorDialogProps;
+    state: ErrorDialogState;
 
     constructor(props: ErrorDialogProps) {
         super(props);
+
+        this.state = {
+            visible: true
+        };
+    }
+
+    hide() {
+        this.setState({visible: false});
     }
 
     render() {
         return (
-            <Modal show={true}>
-                <Modal.Header>
+            <Modal show={this.state.visible} onHide={() => this.hide()}>
+                <Modal.Header closeButton>
                     <Modal.Title>Error</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     {this.errorMessage()}
                 </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => this.hide()}>Close</Button>
+                </Modal.Footer>
             </Modal>
         );
     }
